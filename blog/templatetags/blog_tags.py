3dca90188eb73b5ff67437debfe3f2ec97bbd79a
Adrian Gasior <colid.bondzio@gmail.com>
2021-03-04 12:47:07 +0100

    added rss

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -20,7 +20,7 @@ def show_latest_posts(count=3):
 @register.simple_tag
 def get_most_commented_posts(count=5):
     return Post.published.annotate(
-        total_comments=Count('comment')
+        total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
 
 
